59779f28604fd2a09844bd0a88e5d697acf608ec
Melris ZOHOUN <zohounmelris@gmail.com>
2026-08-10 10:51:47 +0100

    implementation des migraions

--- Gestion/database/migrations/2026_08_10_094148_create_project_user_table.php
+++ Gestion/database/migrations/2026_08_10_094148_create_project_user_table.php
@@ -9,28 +9,28 @@ return new class extends Migration
   /**
    * Run the migrations.
    */
+
   public function up(): void
   {
-
-    Schema::create('projects', function (Blueprint $table) {
-      $table->id();
-
-      $table->foreignId('created_by')
-        ->constrained('users')
+    Schema::create('project_user', function (Blueprint $table) {
+      $table->foreignId('project_id')
+        ->constrained()
         ->cascadeOnDelete();
 
-      $table->string('name');
-      $table->text('description')->nullable();
+      $table->foreignId('user_id')
+        ->constrained()
+        ->cascadeOnDelete();
 
-      $table->timestamps();
+      $table->primary(['project_id', 'user_id']);
     });
   }
+
 
   /**
    * Reverse the migrations.
    */
   public function down(): void
   {
-    Schema::dropIfExists('projects');
+    Schema::dropIfExists('project_user');
   }
 };
